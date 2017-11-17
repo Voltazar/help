39ae82672930766ecb77d7c2ebe1a45134d75cd6
Voltazar <32140020+Voltazar@users.noreply.github.com>
2017-11-17 10:46:20 +0300

    Create 3_newMatrix.cpp

---
+++ 3_newMatrix.cpp
@@ -0,0 +1,12 @@
+#include "lab08.hpp"
+
+float ** newMatrix(int n, int m)
+{
+    float ** matrix = new float*[n];
+    for (int i = 0; i < n, ++i)
+    {
+        matrix[i] = new float[m];
+        memset(matrix[i], 0, m*sizeof(matrix[0]));
+    }
+    return matrix;
+}
