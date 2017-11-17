e21f7042583a92c3cf6b72ac90252dd299c285cc
Voltazar <32140020+Voltazar@users.noreply.github.com>
2017-11-17 11:44:42 +0300

    Update 4_deleteMatrix.cpp

--- 4_deleteMatrix.cpp
+++ 4_deleteMatrix.cpp
@@ -2,11 +2,13 @@
 
 void deleteMatrix(Matrix&)
 {
-    float ** matrix = *m;
+    float ** matrix = m.matrix;
     for (int i = 0; i < n, ++i)
     {
         delete[] matrix[i];
     }
     delete[] matrix;
-    *m= nullptr;
+    m.matrix= nullptr;
+    m.n = 0;
+    m.m = 0;
 }
